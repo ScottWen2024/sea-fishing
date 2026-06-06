b20ddd651fc4f7fa580e7a121a9fdfe97875d0e5
wengh <wengh@fishing.local>
2026-06-06 20:51:52 +0800

    feat: 集成Sentry错误监控

--- fishingMNew/app.js
+++ fishingMNew/app.js
@@ -1,10 +1,17 @@
+﻿const Sentry = require('sentry-miniapp');
+
+Sentry.init({
+  dsn: 'https://0f3f6a619fb7e43c1fc9217493af37a6@o4509009920589824.ingest.de.sentry.io/4511518562058320',
+  tracesSampleRate: 1.0,
+  environment: 'uat'
+});
 App({
   onLaunch() {
     wx.cloud.init({ env: 'cloud1' });
   },
   globalData: {
-    currentPort: '南澳岛',
-    currentCity: '汕头',
+    currentPort: '鍗楁境宀?,
+    currentCity: '姹曞ご',
     currentMode: 'fisherman',
     selectedDate: null,
     weatherCache: {},
