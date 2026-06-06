d00e9b0a77669807d9b9fdda2673f196057258b5
wengh <wengh@fishing.local>
2026-06-06 19:28:24 +0800

    style: Prettier + ESLint 增强 + lint-staged 配置

---
+++ fishingMNew/.eslintrc.js
@@ -0,0 +1,16 @@
+﻿module.exports = {
+  env: { es2021: true, node: true, jest: true, browser: true },
+  parserOptions: { ecmaVersion: "latest" },
+  extends: ["eslint:recommended"],
+  rules: {
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "no-undef": "error",
+    "no-empty": "warn",
+    "no-var": "error",
+    "prefer-const": "warn",
+    "no-console": "off",
+    "no-debugger": "warn",
+    "eqeqeq": ["warn", "always"],
+    "curly": ["warn", "multi-line"],
+  },
+};
